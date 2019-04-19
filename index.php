1ae852057126881642a8028fdb8bbafb637897fb
Victor <webmaster@regalonatural.com>
2019-04-19 09:42:16 +0200

    Convierte el texto introducido en mayusculas-2

--- index.php
+++ index.php
@@ -17,10 +17,10 @@ define("ruta",$_SERVER['DOCUMENT_ROOT'] . "/perletras/images/");
 define("imgfondo","fondo004.jpg");
 
 $nombre=htmlspecialchars($_POST["nombre"]);
-ProcesaPalabra($nombre);
+ProcesaPalabra(strtoupper($nombre));
 
 function ProcesaPalabra ($texto){
-    $nombre=strtoupper($texto);
+    $nombre=$texto;
     $array_letras = str_split($nombre);
     $longitud_array=count($array_letras);
     $nombre_fichero=ruta . $texto. ".png";
